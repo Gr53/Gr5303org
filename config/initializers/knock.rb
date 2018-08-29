2942e03dc11b466d21147b488be6fccf5f94f4f8
Edw536 <edw.536@gmail.com>
2018-08-29 09:02:52 -0500

    cambio en knock para ajustar secret_key_base a pruebas

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -1,7 +1,11 @@
 Knock.setup do |config|
 
+  #config.token_secret_signature_key = ->{
+   # Rails.application.credentials.fetch(:secret_key_base)
+  #}
+
   config.token_secret_signature_key = ->{
-    Rails.application.credentials.fetch(:secret_key_base)
+    "RosaSecret"
   }
 
   ## Expiration claim
